875f8fec21613006b6a94dcaf0970ab3a23b769a
CarolinaCedro <yotechgirl@gmail.com>
2022-11-08 16:44:18 -0300

    up++

---
+++ Iniciantes/Media.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+Nota1 = float(input("Entre com a primeira nota: "))
+Nota2 = float(input("Entre com a segunda nota: "))
+Nota3 = float(input("Entre com a terceira nota: "))
+Nota4 = float(input("Entre com a quarta nota: "))
+
+media = (Nota1+Nota2+Nota3+Nota4)/4
+
+print("A media geral é: ", media)
---
+++ Iniciantes/Soma.py
@@ -0,0 +1,9 @@
+print("Calculadora")
+print("Entre com o primeiro numero: ")
+numero1 = int(input())
+print("Entre com o segundo numero: ")
+numero2 = int(input())
+
+soma = (numero1 + numero2)
+
+print("A soma dos numeros informados é: ", soma)
